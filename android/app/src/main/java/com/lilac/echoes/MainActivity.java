c61f9011e9b4145b4e0d906e048f2d701bd87a1a
Wu Junkai <wu.junkai@qq.com>
2026-05-09 02:21:07 +0800

    ci: configure WebView to allow mixed content

--- android/app/src/main/java/com/lilac/echoes/MainActivity.java
+++ android/app/src/main/java/com/lilac/echoes/MainActivity.java
@@ -1,5 +1,22 @@
 package com.lilac.echoes;
 
+import android.os.Bundle;
+import android.webkit.WebSettings;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // 允许 WebView 加载 HTTP 混合内容
+        if (this.bridge != null && this.bridge.getWebView() != null) {
+            WebSettings settings = this.bridge.getWebView().getSettings();
+            settings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+        }
+    }
+}
