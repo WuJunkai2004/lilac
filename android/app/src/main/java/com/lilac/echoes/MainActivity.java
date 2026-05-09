4ddca9ede473f41bb8959a8b816d02f3cd96d161
Wu Junkai <wu.junkai@qq.com>
2026-05-09 12:36:15 +0800

    chore: update Capacitor network and security configurations
    Updates `VERSION.txt` to 1.1.3.
    Changes Capacitor configuration to use `http` scheme and updates `hostname` to `lilac.app`.
    Adds Content-Security-Policy meta tag to `index.html`.
    Updates Android network security config to permit cleartext traffic for `lilac.app`.
    Removes deprecated mixed content settings from `MainActivity.java`.

--- android/app/src/main/java/com/lilac/echoes/MainActivity.java
+++ android/app/src/main/java/com/lilac/echoes/MainActivity.java
@@ -1,22 +1,5 @@
 package com.lilac.echoes;
 
-import android.os.Bundle;
-import android.webkit.WebSettings;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        // 允许 WebView 加载 HTTP 混合内容
-        if (this.bridge != null && this.bridge.getWebView() != null) {
-            WebSettings settings = this.bridge.getWebView().getSettings();
-            settings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
-        }
-    }
-}
+public class MainActivity extends BridgeActivity {}
