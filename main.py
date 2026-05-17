04c32faabc020df4d1001e49c958ff53e1cec182
Wu Junkai <wu.junkai@qq.com>
2026-05-17 13:07:39 +0800

    feat: change uvicorn reload logic to use dev/debug args
    - Modifies `main.py` to enable uvicorn auto-reload only when 'dev' or 'debug' is passed in sys.argv
    - Reformats uvicorn.run call with explicit line breaks for improved readability

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
+import sys
+
 import uvicorn
 
 
 def main():
-    uvicorn.run("server.main:app", host="0.0.0.0", port=18000, reload=True)
+    uvicorn.run(
+        "server.main:app",
+        host="0.0.0.0",
+        port=18000,
+        reload=("dev" in sys.argv or "debug" in sys.argv),
+    )
 
 
 if __name__ == "__main__":
